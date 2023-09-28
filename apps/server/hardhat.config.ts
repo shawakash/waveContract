dff319a04f709c202b38dec0a1235b6849d28e40
Akash Shaw <shawakash2003@gmail.com>
2023-09-28 07:48:59 +0530

    Deploying to sepolia: 0x1F46a2A44dC23cF403821AF6c4516dd54830aeC9

--- apps/server/hardhat.config.ts
+++ apps/server/hardhat.config.ts
@@ -1,6 +1,19 @@
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const {
+  SEPOLIA_API_URL,
+  SEPOLIA_ACCOUNT_PRIVATE_KEY,
+  PROD_API_URL,
+  MAINNET_PRIVATE_KEY
+} = process.env;
+
+if(!SEPOLIA_API_URL || !SEPOLIA_ACCOUNT_PRIVATE_KEY) {
+  throw Error('Add The Envviorment Variables');
+}
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   //@ts-ignore
@@ -13,6 +26,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_API_URL,
+      accounts: [SEPOLIA_ACCOUNT_PRIVATE_KEY]
+    },
+    mainnet: {
+      url: PROD_API_URL,
+      accounts: [MAINNET_PRIVATE_KEY || ''],
+    }
+  },
 };
 
 export default config;
