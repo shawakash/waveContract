abd029dab3c141bd71da5fdcf9ec21f305f4dba3
Akash Shaw <shawakash2003@gmail.com>
2023-11-08 14:16:32 +0530

    fix: Solana anchor deployment

--- packages/solana/programs/solana/src/lib.rs
+++ packages/solana/programs/solana/src/lib.rs
@@ -4,12 +4,11 @@ declare_id!("2iPwRfZMtBJroE52FUUV4i5Jm75z18KTm3mJPt2N9ZDZ");
 
 #[program]
 pub mod solana {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+  use super::*;
+  pub fn start_stuff_off(_ctx: Context<StartStuffOff>) -> Result <()> {
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff {}
--- packages/solana/tests/solana.ts
+++ packages/solana/tests/solana.ts
@@ -1,16 +1,37 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Solana } from "../../target/types/solana";
+import { Solana } from "../target/types/solana";
 
-describe("solana", () => {
-  // Configure the client to use the local cluster.
+// describe("solana", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
+
+//   const program = anchor.workspace.Solana as Program<Solana>;
+
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const tx = await program.methods.initialize().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
+
+const main = async () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Solana as Program<Solana>;
+  //@ts-ignore
+  const tx = await program.rpc.startStuffOff();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  console.log("📝 Your transaction signature", tx);
+}
+
+(async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
