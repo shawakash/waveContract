fa8dd426b6d4eb72a028711e0bb2079b0e153de4
Akash Shaw <shawakash2003@gmail.com>
2023-11-08 14:06:48 +0530

    Solana Program Debug

--- packages/solana/programs/solana/src/lib.rs
+++ packages/solana/programs/solana/src/lib.rs
@@ -1,31 +1,15 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2iPwRfZMtBJroE52FUUV4i5Jm75z18KTm3mJPt2N9ZDZ");
 
 #[program]
-pub mod myepicproject {
-  use super::*;
-  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
-    // Get a reference to the account.
-    let base_account = &mut ctx.accounts.base_account;
-    // Initialize total_gifs.
-    base_account.total_gifs = 0;
-    Ok(())
-  }
+pub mod solana {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
 }
 
-// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct StartStuffOff<'info> {
-    #[account(init, payer = user, space = 9000)]
-    pub base_account: Account<'info, BaseAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program <'info, System>,
-}
-
-// Tell Solana what we want to store on this account.
-#[account]
-pub struct BaseAccount {
-    pub total_gifs: u64,
-}
+pub struct Initialize {}
--- packages/solana/tests/solana.ts
+++ packages/solana/tests/solana.ts
@@ -1,57 +1,16 @@
-import * as anchor from "@project-serum/anchor";
-const { SystemProgram } = anchor.web3;
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Solana } from "../../target/types/solana";
 
-// describe("solana", () => {
-//   // Configure the client to use the local cluster.
-//   anchor.setProvider(anchor.AnchorProvider.env());
+describe("solana", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
 
-//   const program = anchor.workspace.Solana as Program<Solana>;
+  const program = anchor.workspace.Solana as Program<Solana>;
 
-//   it("Is initialized!", async () => {
-//     // Add your test here.
-//     console.log("From program")
-//     const tx = await program.methods.initialize().rpc();
-//     console.log("Your transaction signature", tx);
-//   });
-// });
-
-const main = async () => {
-  console.log("🚀 Starting test...")
-
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
-
-  const program = anchor.workspace.Solana;
-
-  const baseAccount = anchor.web3.Keypair.generate();
-  
-  console.log(baseAccount.publicKey);
-
-
-  let tx = await program.rpc.initialize({
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-      user: provider.wallet.publicKey,
-      systemProgram: SystemProgram.programId,
-    },
-    signers: [baseAccount],
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
   });
-
-  console.log("📝 Your transaction signature", tx);
-
-  // Fetch data from the account.
-  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF Count', account.totalGifs.toString())
-}
-
-const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-runMain();
+});
